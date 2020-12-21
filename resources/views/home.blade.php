0d8edb6afab9aca70c7bdcf6fe3ab4caa4a780f5
Ivan Mincione <ivan.mincione92@outlook.it>
2020-12-21 15:28:36 +0100

    Init project "laravel-primi-passi"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel primi passi</title>
+    </head>
+    <body>
+
+        <h1>Laravel primi passi</h1>
+    </body>
+</html>
